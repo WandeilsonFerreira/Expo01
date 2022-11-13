326f158d1807dcbd2b937c8b423b384c0abf9e72
WandeilsonFerreira <wandeilson.ferreira@gmail.com>
2022-11-13 15:27:32 -0300

    Atualizando as converssões e calculos

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,15 +16,14 @@ function calculoDistancia(){
   if(qtdExplosivo != '' && gramatura != '' && fr !=''){
     var ple = qtdExplosivo * gramatura;
     var pleTnt = ple * fr;
-    var grains = pleTnt * 15.4;
-    var libras = grains / 7000;
-    var distancia = (18 * (Math.pow(libras,3)))  ;
-    var distanciaMetros = distancia * 0.32;
-    var pressaoInterna = 2410*(libras/(altura*largura*profundidade))*0.72;
+    var pleTntLibras = (pleTnt/453);     
+    var distanciaPes= (18 * Math.cbrt(pleTntLibras))  ;
+    var distanciaMetros = distanciaPes * 0.32;
+    var pressaoInterna = 2410*(pleTntLibras/(altura*largura*profundidade))*0.72;
     
     resultado.innerHTML = `A distância segura é de <strong> ${distanciaMetros.toFixed(2)}</strong> metros aproximadamente.<br><br>Com a utilização do escudo: <strong> ${(distanciaMetros*0.5).toFixed(2)}</strong> metros aproximadamente.<br><br> A presesão interna é de <strong>${(pressaoInterna).toFixed(2)}</strong> p.s.i`;
 
-    if(pressaoInterna > 0 && pressaoInterna  <= 5.0){
+    if(pressaoInterna >= 0 && pressaoInterna  <= 5.0){
       psi.innerHTML = `Efeito da pressão sobre o corpo: <br><strong>Limite para danos auditivos</strong>`
     }
     else if (pressaoInterna >5.0 && pressaoInterna <= 15.0){
@@ -33,10 +32,13 @@ function calculoDistancia(){
     else if (pressaoInterna > 15.0 && pressaoInterna <= 40.0){
       psi.innerHTML = `Efeito da pressão sobre o corpo: <br><strong>Possível chance de danos pulmonais</strong>`
     }
-    else if (pressaoInterna > 40.0 && pressaoInterna <= 100.0){
-      psi.innerHTML = `Efeito da pressão sobre o corpo: <br><strong>Possível chance de morte</strong>`;
+    else if (pressaoInterna > 40.0 && pressaoInterna <= 80.0){
+      psi.innerHTML = `Efeito da pressão sobre o corpo: <br><strong>50% de possibilidade de danos severos aos pulmões</strong>`;
     }
-    else if (pressaoInterna > 100.0 ){
+    else if (pressaoInterna > 80.0 && pressaoInterna <= 200.0){
+      psi.innerHTML = `Efeito da pressão sobre o corpo: <br><strong>Possível chace de morte</strong>`;
+    }
+    else if (pressaoInterna > 200.0 ){
       psi.innerHTML = `Efeito da pressão sobre o corpo: <br><strong>Morte</strong>`
     }
     else{
